e6e9387bc7a50cf7a2d621b3081abd55cff780fc
AdriannaPopko <adunia_93@op.pl>
2016-02-29 08:57:54 +0100

    testy

--- lab1/03Junit/test/com/example/LiczbaRzymskaTest.java
+++ lab1/03Junit/test/com/example/LiczbaRzymskaTest.java
@@ -1,15 +1,39 @@
 package com.example;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class LiczbaRzymskaTest {
-	
-	private LiczbaRzymska rzymska = new LiczbaRzymska(liczba);
 
+public class LiczbaRzymskaTests {
+	LiczbaRzymska rzymska;
+	@Before
+	public void init()
+	{
+		rzymska = new LiczbaRzymska();
+	}
 	@Test
-	public void rzymCheck(){
+	public void liczba10() {
+		rzymska.setLiczba(10);
+		assertEquals("X",rzymska.toString());
+	}
+	@Test
+	public void liczba53() {
+		rzymska.setLiczba(53);
+		assertEquals("LIII",rzymska.toString());
+	}
+	@Test
+	public void liczbaNotNull() {
+		rzymska.setLiczba(27);
+		assertNotNull(rzymska.toString());
+	}
+	@Test
+	public void LiczbaUjemna() {
 		
-		assertEquals("I" ,rzymska.toString(1));
-		
+		rzymska.setLiczba(-4);
+		//nie mialem pomyslu jak ma sie zachowac program gdy sie poda 
+		//liczbe ujemna to dalem by zwracalo null'a
+		assertNull(rzymska.toString());
 	}
 }
